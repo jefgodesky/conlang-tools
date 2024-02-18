8658bb5ebe0e325cc874e5f5a7407cf1ff9e6126
Jason Godesky <jefgodesky@gmail.com>
2024-02-18 18:01:46 -0500

    New class: ConsonantManner

--- phonemes/consonants.py
+++ phonemes/consonants.py
@@ -1,8 +1,42 @@
 from typing import List, Literal, Optional
 
 # Sadly, we can't automate literal-to-list, so if you update this list, make
-# sure you update ConsonantPlace.types to match!
+# sure you update ConsonantPlace.types and/or ConsonantManner.types to match!
 ConsonantPlaceTypes = Literal["labial", "dental", "alveolar-central", "alveolar-lateral", "retroflex", "palatal", "post-alveolar", "velar", "uvular", "pharyngeal", "glottal"]
+ConsonantMannerTypes = Literal["stop", "fricative", "affricate", "nasal", "liquid"]
+ConsonantCategory = Literal["obstruent", "resonant", "n/a"]
+
+
+class ConsonantManner:
+    def __init__(self, value: Optional[ConsonantMannerTypes] = None):
+        types = ConsonantManner.types()
+        self.value = value if value is not None else types[0]
+        if not ConsonantManner.ismanner(self.value):
+            raise TypeError(f"{self.value} is not a valid value for ConsonantManner. Choose one of: {', '.join(types)}.")
+
+    def __eq__(self, other):
+        return self.value == other
+
+    @classmethod
+    def types(cls) -> List[ConsonantMannerTypes]:
+        # Sadly, we can't automate list-to-literal, so if you update this list,
+        # make sure you update ConsonantMannerTypes to match!
+        return ["stop", "fricative", "affricate", "nasal", "liquid"]
+
+    @classmethod
+    def ismanner(cls, candidate: str) -> bool:
+        return candidate in cls.types()
+
+    @staticmethod
+    def category(manner: ConsonantMannerTypes) -> ConsonantCategory:
+        categories = {
+            "stop": "obstruent",
+            "fricative": "obstruent",
+            "affricate": "obstruent",
+            "nasal": "resonant",
+            "liquid": "resonant"
+        }
+        return categories[manner] if manner in categories else "n/a"
 
 
 class ConsonantPlace:
--- tests/phonemes/test_consonant.py
+++ tests/phonemes/test_consonant.py
@@ -1,5 +1,73 @@
 import pytest
-from phonemes.consonants import ConsonantPlace
+from phonemes.consonants import ConsonantManner, ConsonantPlace
+
+
+class TestConsonantManner:
+    def test_creates_consonant_manner(self):
+        manner = ConsonantManner()
+        assert isinstance(manner, ConsonantManner)
+
+    def test_defaults_stop(self):
+        assert ConsonantManner() == "stop"
+
+    def test_can_set_stop(self):
+        assert ConsonantManner("stop") == "stop"
+
+    def test_can_set_fricative(self):
+        assert ConsonantManner("fricative") == "fricative"
+
+    def test_can_set_affricate(self):
+        assert ConsonantManner("affricate") == "affricate"
+
+    def test_can_set_nasal(self):
+        assert ConsonantManner("nasal") == "nasal"
+
+    def test_can_set_liquid(self):
+        assert ConsonantManner("liquid") == "liquid"
+
+    def test_rejects_other(self):
+        with pytest.raises(TypeError):
+            assert ConsonantManner("other") != "other"
+
+    def test_returns_types(self):
+        expected = "stop, fricative, affricate, nasal, liquid"
+        assert ", ".join(ConsonantManner.types()) == expected
+
+    def test_ismanner_stop_true(self):
+        assert ConsonantManner.ismanner("stop") is True
+
+    def test_ismanner_fricative_true(self):
+        assert ConsonantManner.ismanner("fricative") is True
+
+    def test_ismanner_affricate_true(self):
+        assert ConsonantManner.ismanner("affricate") is True
+
+    def test_ismanner_nasal_true(self):
+        assert ConsonantManner.ismanner("nasal") is True
+
+    def test_ismanner_liquid_true(self):
+        assert ConsonantManner.ismanner("liquid") is True
+
+    def test_ismanner_other_false(self):
+        assert ConsonantManner.ismanner("other") is False
+
+    def test_stop_is_obstruent(self):
+        assert ConsonantManner.category("stop") == "obstruent"
+
+    def test_fricative_is_obstruent(self):
+        assert ConsonantManner.category("fricative") == "obstruent"
+
+    def test_affricate_is_obstruent(self):
+        assert ConsonantManner.category("affricate") == "obstruent"
+
+    def test_nasal_is_resonant(self):
+        assert ConsonantManner.category("nasal") == "resonant"
+
+    def test_liquid_is_resonant(self):
+        assert ConsonantManner.category("liquid") == "resonant"
+
+    def test_other_is_na(self):
+        assert ConsonantManner.category("other") == "n/a"
 
 
 class TestConsonantPlace:
