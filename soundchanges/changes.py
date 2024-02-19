ca97e68ee1562288ecc4d276925a058dd2265876
Jason Godesky <jefgodesky@gmail.com>
2024-02-19 11:59:00 -0500

    New sound change methods: vowel_backing, vowel_fronting

--- soundchanges/changes.py
+++ soundchanges/changes.py
@@ -5,17 +5,25 @@ from phonemes.roots import Root
 from utils.methods import oxford_comma, get_choices
 
 
-def vowel_raise_lower(
-    lang: Language, syllables: Optional[str] = None, rise: bool = True
+def vowel_change(
+    lang: Language,
+    map_type: str = "height",
+    syllables: Optional[str] = None,
+    reverse: bool = True,
 ) -> Tuple[str, List[str]]:
     rand_all = "all" if random.random() < 0.1 else "stressed"
     affected = syllables if syllables is not None else rand_all
-    mapping = lang.vowel_height_mapping(rise)
+    mapping = lang.vowel_mapping(map_type, reverse=reverse)
     affected_keys = [key for key in mapping.keys() if key != mapping[key].symbol]
     changes = [f"/{key}/ became /{mapping[key].symbol}/" for key in affected_keys]
-    direction = "Lower" if rise is False else "Raise"
+    height_direction = "Lowering" if reverse is False else "Raising"
+    loc_direction = "Backing" if reverse is False else "Fronting"
+    direction = loc_direction if map_type == "location" else height_direction
     title = f"**Vowel {direction}:**"
-    description = f"{title} {oxford_comma(changes)} in {affected} syllables."
+    affects_desc = f"{oxford_comma(changes)} in {affected} syllables."
+    no_changes = len(affected_keys) < 1
+    affects = "No changes." if no_changes else affects_desc
+    description = f"{title} {affects}"
 
     new_words: List[str] = []
     for original in lang.words:
@@ -31,25 +39,42 @@ def vowel_raise_lower(
     return description, new_words
 
 
-def vowel_lower(
+def vowel_lowering(
     lang: Language, syllables: Optional[str] = None
 ) -> Tuple[str, List[str]]:
-    return vowel_raise_lower(lang, syllables, rise=False)
+    return vowel_change(lang, "height", syllables, reverse=False)
 
 
-def vowel_raise(
+def vowel_raising(
     lang: Language, syllables: Optional[str] = None
 ) -> Tuple[str, List[str]]:
-    return vowel_raise_lower(lang, syllables)
+    return vowel_change(lang, "height", syllables)
+
+
+def vowel_fronting(
+    lang: Language, syllables: Optional[str] = None
+) -> Tuple[str, List[str]]:
+    return vowel_change(lang, "location", syllables)
+
+
+def vowel_backing(
+    lang: Language, syllables: Optional[str] = None
+) -> Tuple[str, List[str]]:
+    return vowel_change(lang, "location", syllables, reverse=False)
 
 
 def change(lang: Language) -> Tuple[str, List[str]]:
-    sound_changes = {"vowel_lower": 7, "vowel_raise": 7}
-    sound_change = get_choices(sound_changes)
+    sound_changes = {
+        "vowel_backing": (6, vowel_backing),
+        "vowel_fronting": (6, vowel_fronting),
+        "vowel_lowering": (7, vowel_lowering),
+        "vowel_raising": (7, vowel_raising),
+    }
 
-    if sound_change == "vowel_lower":
-        return vowel_lower(lang)
-    elif sound_change == "vowel_raise":
-        return vowel_raise(lang)
+    choices = get_choices({key: wgt for key, (wgt, _) in sound_changes.items()})
+    sound_change = random.choice(choices)
+    if sound_change in sound_changes:
+        _, change_fn = sound_changes[sound_change]
+        return change_fn(lang)
     else:
         return "No change.", lang.words
--- tests/soundchanges/test_changes.py
+++ tests/soundchanges/test_changes.py
@@ -1,18 +1,94 @@
 import pytest
 from language.classes import Phonology, Phonotactics, Language
-from soundchanges.changes import change, vowel_lower, vowel_raise
+from soundchanges.changes import (
+    change,
+    vowel_backing,
+    vowel_fronting,
+    vowel_lowering,
+    vowel_raising,
+)
 
 
-class TestVowelLower:
+class TestVowelBacking:
+    @pytest.fixture
+    def example_language(self):
+        nucleus = {"a": 1, "ɑ": 1, "i": 1, "u": 1}
+        pt = Phonotactics(onset={"b": 1}, nucleus=nucleus, coda={})
+        pl = Phonology(stress="initial", openness=1)
+        return Language(phonotactics=pt, phonology=pl, words=["/ˈba.ba/", "/ˈbɑ.ba/"])
+
+    def test_vowel_backing(self, example_language):
+        description, words = vowel_backing(example_language, syllables="all")
+        exdesc_title = "**Vowel Backing:** "
+        exdesc_changes = "/a/ became /ɑ/"
+        exdesc_syllables = " in all syllables."
+        expected_desc = exdesc_title + exdesc_changes + exdesc_syllables
+        assert description == expected_desc
+        assert words[0] == "/ˈbɑ.bɑ/"
+        assert words[1] == "/ˈbɑ.bɑ/"
+
+    def test_vowel_backing_stressed(self, example_language):
+        description, words = vowel_backing(example_language, syllables="stressed")
+        exdesc_title = "**Vowel Backing:** "
+        exdesc_changes = "/a/ became /ɑ/"
+        exdesc_syllables = " in stressed syllables."
+        expected_desc = exdesc_title + exdesc_changes + exdesc_syllables
+        assert description == expected_desc
+        assert words[0] == "/ˈbɑ.ba/"
+        assert words[1] == "/ˈbɑ.ba/"
+
+    def test_vowel_backing_randomized(self, example_language):
+        description, _ = vowel_backing(example_language)
+        stressed_syllables = "stressed syllables" in description
+        all_syllables = "all syllables" in description
+        assert stressed_syllables or all_syllables
+
+
+class TestVowelFronting:
+    @pytest.fixture
+    def example_language(self):
+        nucleus = {"a": 1, "ɑ": 1, "i": 1, "u": 1}
+        pt = Phonotactics(onset={"b": 1}, nucleus=nucleus, coda={})
+        pl = Phonology(stress="initial", openness=1)
+        return Language(phonotactics=pt, phonology=pl, words=["/ˈbɑ.bɑ/", "/ˈba.ba/"])
+
+    def test_vowel_fronting(self, example_language):
+        description, words = vowel_fronting(example_language, syllables="all")
+        exdesc_title = "**Vowel Fronting:** "
+        exdesc_changes = "/ɑ/ became /a/"
+        exdesc_syllables = " in all syllables."
+        expected_desc = exdesc_title + exdesc_changes + exdesc_syllables
+        assert description == expected_desc
+        assert words[0] == "/ˈba.ba/"
+        assert words[1] == "/ˈba.ba/"
+
+    def test_vowel_fronting_stressed(self, example_language):
+        description, words = vowel_fronting(example_language, syllables="stressed")
+        exdesc_title = "**Vowel Fronting:** "
+        exdesc_changes = "/ɑ/ became /a/"
+        exdesc_syllables = " in stressed syllables."
+        expected_desc = exdesc_title + exdesc_changes + exdesc_syllables
+        assert description == expected_desc
+        assert words[0] == "/ˈba.bɑ/"
+        assert words[1] == "/ˈba.ba/"
+
+    def test_vowel_fronting_randomized(self, example_language):
+        description, _ = vowel_fronting(example_language)
+        stressed_syllables = "stressed syllables" in description
+        all_syllables = "all syllables" in description
+        assert stressed_syllables or all_syllables
+
+
+class TestVowelLowering:
     @pytest.fixture
     def example_language(self):
         pt = Phonotactics(onset={"b": 1}, nucleus={"a": 1, "e": 1, "i": 1}, coda={})
         pl = Phonology(stress="initial", openness=1)
         return Language(phonotactics=pt, phonology=pl, words=["/ˈba.be/", "/ˈbe.bi/"])
 
-    def test_vowel_lower(self, example_language):
-        description, words = vowel_lower(example_language, syllables="all")
-        exdesc_title = "**Vowel Lower:** "
+    def test_vowel_lowering(self, example_language):
+        description, words = vowel_lowering(example_language, syllables="all")
+        exdesc_title = "**Vowel Lowering:** "
         exdesc_changes = "/i/ became /e/ and /e/ became /a/"
         exdesc_syllables = " in all syllables."
         expected_desc = exdesc_title + exdesc_changes + exdesc_syllables
@@ -20,9 +96,9 @@ class TestVowelLower:
         assert words[0] == "/ˈba.ba/"
         assert words[1] == "/ˈba.be/"
 
-    def test_vowel_lower_stressed(self, example_language):
-        description, words = vowel_lower(example_language, syllables="stressed")
-        exdesc_title = "**Vowel Lower:** "
+    def test_vowel_lowering_stressed(self, example_language):
+        description, words = vowel_lowering(example_language, syllables="stressed")
+        exdesc_title = "**Vowel Lowering:** "
         exdesc_changes = "/i/ became /e/ and /e/ became /a/"
         exdesc_syllables = " in stressed syllables."
         expected_desc = exdesc_title + exdesc_changes + exdesc_syllables
@@ -30,23 +106,23 @@ class TestVowelLower:
         assert words[0] == "/ˈba.be/"
         assert words[1] == "/ˈba.bi/"
 
-    def test_vowel_lower_randomized(self, example_language):
-        description, _ = vowel_lower(example_language)
+    def test_vowel_lowering_randomized(self, example_language):
+        description, _ = vowel_lowering(example_language)
         stressed_syllables = "stressed syllables" in description
         all_syllables = "all syllables" in description
         assert stressed_syllables or all_syllables
 
 
-class TestVowelRaise:
+class TestVowelRaising:
     @pytest.fixture
     def example_language(self):
         pt = Phonotactics(onset={"b": 1}, nucleus={"a": 1, "e": 1, "i": 1}, coda={})
         pl = Phonology(stress="initial", openness=1)
         return Language(phonotactics=pt, phonology=pl, words=["/ˈba.be/", "/ˈbe.bi/"])
 
-    def test_vowel_raise(self, example_language):
-        description, words = vowel_raise(example_language, syllables="all")
-        exdesc_title = "**Vowel Raise:** "
+    def test_vowel_raising(self, example_language):
+        description, words = vowel_raising(example_language, syllables="all")
+        exdesc_title = "**Vowel Raising:** "
         exdesc_changes = "/a/ became /e/ and /e/ became /i/"
         exdesc_syllables = " in all syllables."
         expected_desc = exdesc_title + exdesc_changes + exdesc_syllables
@@ -54,9 +130,9 @@ class TestVowelRaise:
         assert words[0] == "/ˈbe.bi/"
         assert words[1] == "/ˈbi.bi/"
 
-    def test_vowel_raise_stressed(self, example_language):
-        description, words = vowel_raise(example_language, syllables="stressed")
-        exdesc_title = "**Vowel Raise:** "
+    def test_vowel_raising_stressed(self, example_language):
+        description, words = vowel_raising(example_language, syllables="stressed")
+        exdesc_title = "**Vowel Raising:** "
         exdesc_changes = "/a/ became /e/ and /e/ became /i/"
         exdesc_syllables = " in stressed syllables."
         expected_desc = exdesc_title + exdesc_changes + exdesc_syllables
@@ -64,8 +140,8 @@ class TestVowelRaise:
         assert words[0] == "/ˈbe.be/"
         assert words[1] == "/ˈbi.bi/"
 
-    def test_vowel_raise_randomized(self, example_language):
-        description, _ = vowel_raise(example_language)
+    def test_vowel_raising_randomized(self, example_language):
+        description, _ = vowel_raising(example_language)
         stressed_syllables = "stressed syllables" in description
         all_syllables = "all syllables" in description
         assert stressed_syllables or all_syllables
