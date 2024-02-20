65ca820c6765286d1b62a9e931950cd885f0b128
Jason Godesky <jefgodesky@gmail.com>
2024-02-20 12:37:10 -0500

    Root: Use fixture to reduce duplicated example setup

--- tests/phonemes/test_root.py
+++ tests/phonemes/test_root.py
@@ -1,33 +1,36 @@
+import pytest
+
 from phonemes.phonemes import get_phoneme
 from phonemes.roots import Root, Syllable
 
 
 class TestRoot:
-    def test_creates_root(self):
-        root = Root("/ˈba.ba/")
-        assert isinstance(root, Root)
+    @pytest.fixture
+    def example_root(self):
+        return Root("/ˈba.ba/")
 
-    def test_takes_ipa(self):
-        root = Root("/ˈba.ba/")
-        assert root.ipa == "/ˈba.ba/"
+    def test_creates_root(self, example_root):
+        assert isinstance(example_root, Root)
 
-    def test_breaks_down_syllables(self):
-        root = Root("/ˈba.ba/")
-        assert len(root.syllables) == 2
-        assert all([isinstance(syllable, Syllable) for syllable in root.syllables])
-        assert root.syllables[0].stressed is True
-        assert root.syllables[1].stressed is False
+    def test_takes_ipa(self, example_root):
+        assert example_root.ipa == "/ˈba.ba/"
 
-    def test_rebuild(self):
-        root = Root("/ˈba.ba/")
-        root.syllables[0].phonemes[0] = get_phoneme("k")
-        root.syllables[1].phonemes[0] = get_phoneme("l")
-        root.rebuild()
-        assert root.ipa == "/ˈka.la/"
+    def test_breaks_down_syllables(self, example_root):
+        assert len(example_root.syllables) == 2
+        assert all(
+            [isinstance(syllable, Syllable) for syllable in example_root.syllables]
+        )
+        assert example_root.syllables[0].stressed is True
+        assert example_root.syllables[1].stressed is False
 
-    def test_phonemes(self):
-        root = Root("/ˈba.ba/")
-        symbols = [p.symbol for p in root.phonemes]
+    def test_rebuild(self, example_root):
+        example_root.syllables[0].phonemes[0] = get_phoneme("k")
+        example_root.syllables[1].phonemes[0] = get_phoneme("l")
+        example_root.rebuild()
+        assert example_root.ipa == "/ˈka.la/"
+
+    def test_phonemes(self, example_root):
+        symbols = [p.symbol for p in example_root.phonemes]
         assert "".join(symbols) == "baba"
 
 
