714b191cbf227c1c4bc1ba635eaa330af6064ba0
Jason Godesky <jefgodesky@gmail.com>
2024-02-23 09:48:25 -0500

    Add "Create Language" tool

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import argparse
 import glob
 import os
+import yaml
 from language.classes import Language
 from soundchanges.history import History
 
 if __name__ == "__main__":
     desc = {
         "main": "Command line tools for working with constructed languages (conlangs).",
-        "lang": "Which language you’d like to work with. Options are: ",
-        "tool": "Which tool do you want to use? Options are 'words' or 'history'.",
+        "tool": "Which tool do you want to use? Options are 'words', 'history', "
+        "and 'create'.",
+        "lang": "[Word Generator/History] Which language you’d like to work with. "
+        "Options are: ",
         "changes": "[History] How many sound changes do you want to model?",
         "csv": "[History] Filename to which you’d like to write the history of how "
         "each word in the language changed (CSV format).",
         "log": "[History] Filename to which you’d like to write the history of the "
         "changes that occurred (Markdown format).",
+        "name": "[Create Language] The name of the language you would like to create.",
+        "wordlist": "[Create Language] Name of a file containing words from which you’d "
+        "like to create a new language. This should be a text file with a new word on "
+        "each line, spelled in IPA (International Phonetic Alphabet).",
         "words": "[Word Generator] When using the word generator, tool, how many words"
         "would you like to generate?",
     }
@@ -25,18 +32,26 @@ if __name__ == "__main__":
     desc["lang"] = desc["lang"] + ", ".join(language_options)
 
     parser = argparse.ArgumentParser(description=desc["main"])
-    parser.add_argument("--lang", "-l", type=str, help=desc["lang"])
     parser.add_argument("--tool", "-t", type=str, help=desc["tool"])
+    parser.add_argument("--lang", "-l", type=str, help=desc["lang"])
     parser.add_argument("--changes", "-c", type=int, help=desc["changes"])
     parser.add_argument("--csv", type=str, help=desc["csv"])
     parser.add_argument("--log", type=str, help=desc["log"])
+    parser.add_argument("--name", "-n", type=str, help=desc["name"])
+    parser.add_argument("--wordlist", "-wl", type=str, help=desc["wordlist"])
     parser.add_argument("--words", "-w", type=int, help=desc["words"])
 
     args = parser.parse_args()
-    if args.lang:
-        lang = Language.load(args.lang)
+    nolang_msg = (
+        "No language specified. Please use '--lang' or '-l' to specify a language."
+    )
 
-        if args.tool == "changes":
+    if args.tool == "history":
+        # History
+        if not args.lang:
+            print(nolang_msg)
+        else:
+            lang = Language.load(args.lang)
             num_changes = args.changes or 1
             history = History(lang)
             for _ in range(num_changes):
@@ -55,12 +70,29 @@ if __name__ == "__main__":
             record = f"recorded in {csv_filename} and {log_filename}."
             print(f"{args.lang} underwent {num_changes} changes, {record}.")
 
+    elif args.tool == "create":
+        # Create Language
+        if not args.wordlist:
+            print(
+                "No word list specified. Please use '--worldist' or '-wl' to specify "
+                "a file to use for your word list."
+            )
         else:
+            name = args.name or "new_language"
+            with open(args.wordlist, "r", encoding="utf-8") as wordlist_file:
+                words = [line.strip() for line in wordlist_file.readlines()]
+                lang = Language.from_words(words)
+                with open(f"languages/{name}.yaml", "w", encoding="utf-8") as yaml_file:
+                    yaml.safe_dump(lang.to_dict(), yaml_file, allow_unicode=True)
+                    print(f"Created languages/{name}.yaml")
+
+    else:
+        # Word Generator
+        if not args.lang:
+            print(nolang_msg)
+        else:
+            lang = Language.load(args.lang)
             num_words = args.words or 10
             new_words = lang.generate_new_words(num_words)
             for word in new_words:
                 print(word)
-    else:
-        print(
-            "No language specified. Please use '--lang' or '-l' to specify a language."
-        )
